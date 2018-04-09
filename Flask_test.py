3ae04c8df787b8e78a83f4f1232051c21123c3ee
Honcharov12 <vovangon12@gmail.com>
2018-04-09 09:57:03 +0300

    Clean up

--- Flask_test.py
+++ Flask_test.py
@@ -4,7 +4,7 @@ from flask import render_template, flash
 from bigquery import get_client
 from flask import (request, jsonify)
 from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField, StringField
+from wtforms import SubmitField, StringField
 from wtforms.validators import DataRequired
 
 import os
@@ -13,18 +13,8 @@ import time
 import ast
 import datetime
 
-choices_x = [("ProductID", "ProductID"), ("Quantity", "Quantity"), ("Price", "Price"), ("ProductName", "ProductName"), ("DCSS", "DCSS")]
-choices_y = [("ProductID", "ProductID"), ("Quantity", "Quantity"), ("Price", "Price")]
-n_choices_x = ("ProductID", "Quantity", "Price", "ProductName", "DCSS")
-n_choices_y = ("ProductID)", "Quantity", "Price")
-
 class StringForm(FlaskForm):
     department = StringField("Department: ")
-    submit = SubmitField("Get Data")
-
-class SelectForm(FlaskForm):
-    x_axes = SelectField("Choice 1: ", choices = choices_x)
-    y_axes = SelectField("Choice 2: ", choices = choices_y)
     submit = SubmitField("Get Data")
 
 def wait_until(somepredicate, timeout, period=0.25, *args, **kwargs):
@@ -81,22 +71,6 @@ def get_dates(department):
 
 DEPARTMENTS = get_departments()
 
-def qtest(first, second):
-    client = get_client(json_key_file=json_key, readonly=True)
-
-    # Submit an async query.
-    job_id, _results = client.query('SELECT %s, %s FROM [bamboo-creek-195008:test2.OleSmokey3] LIMIT 10' % (first, second))
-
-    # Check if the query has finished running.
-    complete, row_count = client.check_job(job_id)
-
-    wait_until(lambda c=client, id=job_id: client.check_job(id), 10000)
-
-    results = client.get_query_rows(job_id)
-    return str(results)
-
-
-
 @app.route('/query2', methods = ['GET', 'POST'])
 def queryPage2():
     form = StringForm()
@@ -116,39 +90,5 @@ def queryPage2():
 
     return render_template('query_page2.html', form=form)
 
-@app.route('/query', methods = ['GET', 'POST'])
-def queryPage():
-    form = SelectForm()
-
-    if request.method == 'POST':
-        if form.x_axes.data == form.y_axes.data:
-            flash("Same fields are not required")
-            return render_template('query_page.html', form = form)
-
-        values, labels = executeQuery(form.x_axes.data, form.y_axes.data)
-        return render_template('chart.html', values=values, labels=labels)
-
-    return render_template('query_page.html', form=form)
-
-
-# not implemented
-def executeQuery(x, y):
-    s = qtest(x, y)
-    data = ast.literal_eval(s)
-    labels = []
-    values = []
-    for field in data:
-        labels.append(field[x])
-        values.append(field[y])
-    return labels, values
-
-
-@app.route('/ajax', methods=['POST'])
-def ajax_request():
-    query = request.form['query']
-    res = executeQuery(query)
-    return jsonify(returnedData=res)
-
-
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8001)
+    app.run()
